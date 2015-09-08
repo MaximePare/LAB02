3474fe6079014d4553c5d4e6b9440f69264119a6
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-08 16:54:04 -0400

    Tout fonctionne mais la façon dans je change la couleur des bullets reste à désirer. Il doit y avoir une autre manière de faire mais je n'ait plus le gout de chercher.

--- carouselPhotoScript.js
+++ carouselPhotoScript.js
@@ -10,16 +10,24 @@ function carousel(numberClick){
     if(numberImage < 1){
         numberImage = total;
     }
-    Image.src = "Images/"+ numberImage +".jpg";
+    image.src = "Images/"+ numberImage +".jpg";
 }
-window.setInterval(function carouselWithTimer(){
+window.setInterval(function carouselWithTimer() {
     var image = document.getElementById('img');
-    numberImage = numberImage + numberClick;
-    if(numberImage > total){
-        numberImage = 1;
+    numberImage = numberImage + 1;
+    if(numberImage > total){numberImage = 1;}
+    if(numberImage < 1){numberImage = total;}
+    image.src = "Images/"+ numberImage +".jpg";
+},5000);
+
+function bulletChange(numberBullet){
+    var image = document.getElementById('img');
+    var imageBulletClick = document.getElementById('bulletImg'+ numberBullet);
+    numberImage = numberBullet;
+    image.src = "Images/"+ numberImage +".jpg";
+    for(i = 1; i < 8;i++){
+        var imageBullet = document.getElementById('bulletImg'+ i);
+        imageBullet.src = "Images/whiteBullet.png";
     }
-    if(numberImage < 1){
-        numberImage = total;
-    }
-    Image.src = "Images/"+ numberImage +".jpg";
-},50);
+    imageBulletClick.src = "Images/blueBullet.png";
+}
