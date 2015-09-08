648659896fd935014252882609a1454f8c257379
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-08 17:01:12 -0400

    Problème au niveau des bullets qui ne suivait pas le numéro de l'image de règle. La tout semble fonctionner.

--- carouselPhotoScript.js
+++ carouselPhotoScript.js
@@ -15,6 +15,7 @@ function carousel(numberClick){
 window.setInterval(function carouselWithTimer() {
     var image = document.getElementById('img');
     numberImage = numberImage + 1;
+    bulletChange(numberImage);
     if(numberImage > total){numberImage = 1;}
     if(numberImage < 1){numberImage = total;}
     image.src = "Images/"+ numberImage +".jpg";
