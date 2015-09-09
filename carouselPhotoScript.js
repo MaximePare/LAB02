0467773d4060cd45f9aa6f6167342908ab03cba0
Roxanne Landry <roxanne.landry.2@ulaval.ca>
2015-09-08 20:14:00 -0400

    Les problèmes relevés ont été corrigés. au niveau de précédent et suivant.

--- carouselPhotoScript.js
+++ carouselPhotoScript.js
@@ -10,14 +10,15 @@ function carousel(numberClick){
     if(numberImage < 1){
         numberImage = total;
     }
+    bulletChange(numberImage);
     image.src = "Images/"+ numberImage +".jpg";
 }
 window.setInterval(function carouselWithTimer() {
     var image = document.getElementById('img');
     numberImage = numberImage + 1;
-    bulletChange(numberImage);
     if(numberImage > total){numberImage = 1;}
     if(numberImage < 1){numberImage = total;}
+    bulletChange(numberImage);
     image.src = "Images/"+ numberImage +".jpg";
 },5000);
 
